5a764063d2ab9436520f2053790afc045fbb7e38
msemialjac <mario.semialjac@icloud.com>
2021-12-20 15:25:40 +0100

    added search listings functionality

---
+++ listings/choices.py
@@ -0,0 +1,48 @@
+bedroom_choices = {
+  '1':1,
+  '2':2,
+  '3':3,
+  '4':4,
+  '5':5,
+  '6':6,
+  '7':7,
+  '8':8,
+  '9':9,
+  '10':10
+  }
+
+price_choices = {
+  '100000':'EUR 100,000',
+  '200000':'EUR 200,000',
+  '300000':'EUR 300,000',
+  '400000':'EUR 400,000',
+  '500000':'EUR 500,000',
+  '600000':'EUR 600,000',
+  '700000':'EUR 700,000',
+  '800000':'EUR 800,000',
+  '900000':'EUR 900,000',
+  '1000000':'EUR 1M+',
+}
+
+state_choices = {
+        'BB': 'Bjelovarsko bilogorska',
+        'BP': 'Brodsko posavska',
+        'DN': 'Dubrovačko neretvanska',
+        'IS': 'Istarska',
+        'KA': 'Karlovačka',
+        'KK': 'Koprivničko križevačka',
+        'KZ': 'Krapinsko zagorska',
+        'LS': 'Ličko senjska',
+        'ME': 'Međimurska',
+        'OB': 'Osječko baranjska',
+        'PS': 'Požeško slavonska',
+        'ŠK': 'Šibensko kninska',
+        'SM': 'Sisačko moslovačka',
+        'SD': 'Splitsko dalmatinska',
+        'VA': 'Varaždinska',
+        'VP': 'Virovitčko podravska',
+        'VS': 'Vukovarsko srijemska',
+        'ZD': 'Zadarska',
+        'GZ': 'Grad Zagreb',
+        'ZG': 'Zagrebačka',
+}
